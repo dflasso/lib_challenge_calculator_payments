540eef299453e7cf7d97f53a7db8a127145f07d2
dlasso <dlasso@bigdataca.com>
2022-06-26 19:01:37 -0500

    completed gitlab-cicd

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 setup(
     name='lib_pay_calculator',
     packages=find_packages(include=['lib_pay_calculator']),
-    version='0.0.1',
+    version='0.0.2',
     description='Python library that calculate payments according day and schedule of hours worked',
     author='Dany Lasso',
     author_email='dannylasso.a@gmail.com',
